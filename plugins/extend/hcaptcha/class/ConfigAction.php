096686486c422ed2f91e445a673981140473e207
jDanek <jirka.danek@jdanek.eu>
2024-12-07 18:13:49 +0100

    Update version. Replace inputs, textareas and selects with system Form class methods

--- plugins/extend/hcaptcha/class/ConfigAction.php
+++ plugins/extend/hcaptcha/class/ConfigAction.php
@@ -10,24 +10,24 @@ class ConfigAction extends BaseConfigAction
 {
     protected function getFields(): array
     {
-        $config = $this->plugin->getConfig();
+        $config = $this->plugin->getConfig();dump(Form::loadCheckbox('dark_mode', $config['dark_mode'], 'config'));
 
         return [
             'site_key' => [
                 'label' => _lang('hcaptcha.site_key'),
-                'input'=> '<input type="text" name="config[site_key]" value="' . Request::post('site_key', $config['site_key']) . '" class="inputbig">',
-                'type' => 'text'
+                'input' => Form::input('text', 'config[site_key]', Request::post('site_key', $config['site_key']), ['class' => 'inputbig']),
+                'type' => 'text',
             ],
             'secret_key' => [
                 'label' => _lang('hcaptcha.secret_key'),
-                'input'=> '<input type="text" name="config[secret_key]" value="' . Request::post('secret_key', $config['secret_key']) . '" class="inputbig" placeholder="0x....">',
-                'type' => 'text'
+                'input' => Form::input('text', 'config[secret_key]', Request::post('secret_key', $config['secret_key']), ['class' => 'inputbig', 'placeholder' => '0x....']),
+                'type' => 'text',
             ],
             'dark_mode' => [
                 'label' => _lang('hcaptcha.dark_mode'),
-                'input' => '<input type="checkbox" name="config[dark_mode]" value="1"' . Form::loadCheckbox('config', $config['dark_mode'], 'dark_mode')) . '>',
-                'type' => 'checkbox'
-            ]
+                'input' => Form::input('checkbox', 'config[dark_mode]', '1', ['checked' => Form::loadCheckbox('config', $config['dark_mode'], 'dark_mode')]),
+                'type' => 'checkbox',
+            ],
         ];
     }
 }
